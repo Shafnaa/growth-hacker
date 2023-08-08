e0cef95ff0a9f0043a633071e4c32a49e5730c50
Shafnaa <saujanashafi2003@gmail.com>
2023-08-08 16:26:21 +0700

    growth hacker first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        black: "#130C32",
+        gray: "#413C5A",
+        orange: "#FF5802",
+        smoke: "#E4E4E4",
+      },
+      backgroundImage: {
+        "footer-newsletter":
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0.80) 100%), url('https://s3-alpha-sig.figma.com/img/77eb/d5d9/085bc2ede3b97825f92bcac348e06e92?Expires=1692576000&Signature=WmuK~Ceh-~ozVlo7j~C0KZdbFoeaaeCks8g~sCOkZzH4S4JsYMXwQr~CdGF7hDFFBeBDxssHP99nOq05alvtYRdUzYEpgYmS22W2Ge~naSn4xEtVVxNmhlo8wKhXotu6DxsTQbtTrwZMiXIO62-Kt02D4juaCNVGOFzPcDoAvFsnPePOgkfK0FB-flnFkoerMg4yhAp-PwHb383k9-OFx1H~rRab0k3e0Tur3YGiVHGceIhYrIUe9TiL1kHhzj~8euhZityq4-mTdFWVkCpjAg3iFL6pSnxJxHbdimjOEcvMIhV1ZeecpWKW89fgg2OHwm0JQyE1EMRHoX~Y7sdV3Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')",
+        recipes:
+          "linear-gradient(180deg, rgba(0, 0, 0, 0.70) 0%, rgba(0, 0, 0, 0.00) 100%), linear-gradient(0deg, #1E1E1E 0%, rgba(30, 30, 30, 0.00) 100%), url('https://s3-alpha-sig.figma.com/img/77eb/d5d9/085bc2ede3b97825f92bcac348e06e92?Expires=1692576000&Signature=WmuK~Ceh-~ozVlo7j~C0KZdbFoeaaeCks8g~sCOkZzH4S4JsYMXwQr~CdGF7hDFFBeBDxssHP99nOq05alvtYRdUzYEpgYmS22W2Ge~naSn4xEtVVxNmhlo8wKhXotu6DxsTQbtTrwZMiXIO62-Kt02D4juaCNVGOFzPcDoAvFsnPePOgkfK0FB-flnFkoerMg4yhAp-PwHb383k9-OFx1H~rRab0k3e0Tur3YGiVHGceIhYrIUe9TiL1kHhzj~8euhZityq4-mTdFWVkCpjAg3iFL6pSnxJxHbdimjOEcvMIhV1ZeecpWKW89fgg2OHwm0JQyE1EMRHoX~Y7sdV3Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')",
+      },
+    },
+  },
+};
